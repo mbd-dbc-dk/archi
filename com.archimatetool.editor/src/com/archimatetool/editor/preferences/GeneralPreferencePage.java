92a008616240c2ee023c53bf2b472ed0ea7f7001
Phillipus <p.beauvoir@dadabeatnik.com>
2014-03-11 18:47:11 +0000

    [e4] Support for Themes
    * Add Preference to change UI theme
    * Add trial Archi CSS Theme
    * Use theme css styles from latest Eclipse I build
    * Add themes plugin

--- com.archimatetool.editor/src/com/archimatetool/editor/preferences/GeneralPreferencePage.java
+++ com.archimatetool.editor/src/com/archimatetool/editor/preferences/GeneralPreferencePage.java
@@ -5,7 +5,22 @@
  */
 package com.archimatetool.editor.preferences;
 
+import java.util.List;
+
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.ui.css.swt.theme.ITheme;
+import org.eclipse.e4.ui.css.swt.theme.IThemeEngine;
+import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -16,7 +31,6 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPreferenceConstants;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.PlatformUI;
 
@@ -25,6 +39,7 @@ import org.eclipse.ui.PlatformUI;
  * 
  * @author Phillip Beauvoir
  */
+@SuppressWarnings({ "restriction", "nls" })
 public class GeneralPreferencePage
 extends PreferencePage
 implements IWorkbenchPreferencePage, IPreferenceConstants {
@@ -34,11 +49,14 @@ implements IWorkbenchPreferencePage, IPreferenceConstants {
     private Button fBackupOnSaveButton;
     
     private Spinner fMRUSizeSpinner;
-    
-    private Button fUseCurvedTabsButton;
-    
     private Button fAnimateVisualiserNodesButton;
     
+    private ComboViewer fThemeComboViewer;
+    
+    private IThemeEngine fThemeEngine;
+    private ITheme fCurrentTheme;
+    private String fDefaultTheme;
+
 	public GeneralPreferencePage() {
 		setPreferenceStore(Preferences.STORE);
 	}
@@ -86,11 +104,33 @@ implements IWorkbenchPreferencePage, IPreferenceConstants {
         appearanceGroup.setLayout(new GridLayout(2, false));
         appearanceGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
         
-        fUseCurvedTabsButton = new Button(appearanceGroup, SWT.CHECK);
-        fUseCurvedTabsButton.setText(Messages.GeneralPreferencePage_4);
+        // Themes
+        label = new Label(appearanceGroup, SWT.NULL);
+        label.setText("Theme: ");
+        fThemeComboViewer = new ComboViewer(appearanceGroup, SWT.READ_ONLY);
         gd = new GridData(GridData.FILL_HORIZONTAL);
-        gd.horizontalSpan = 2;
-        fUseCurvedTabsButton.setLayoutData(gd);
+        fThemeComboViewer.getCombo().setLayoutData(gd);
+        
+        fThemeComboViewer.setContentProvider(new IStructuredContentProvider() {
+            public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+            }
+            
+            public void dispose() {
+            }
+            
+            public Object[] getElements(Object inputElement) {
+                return (Object[])inputElement;
+            }
+        });
+        
+        fThemeComboViewer.setLabelProvider(new LabelProvider() {
+            @Override
+            public String getText(Object element) {
+                return ((ITheme)element).getLabel();
+            }
+        });
+        
+        fThemeComboViewer.setSorter(new ViewerSorter());
         
         // Visualiser
         Group visualiserGroup = new Group(client, SWT.NULL);
@@ -106,6 +146,15 @@ implements IWorkbenchPreferencePage, IPreferenceConstants {
         
         setValues();
         
+        fThemeComboViewer.addSelectionChangedListener(new ISelectionChangedListener() {
+            public void selectionChanged(SelectionChangedEvent event) {
+                ITheme theme = (ITheme)((IStructuredSelection)fThemeComboViewer.getSelection()).getFirstElement();
+                if(theme != null && theme != fThemeEngine.getActiveTheme()) {
+                    fThemeEngine.setTheme(theme, true);
+                }
+            }
+        });
+        
         return client;
     }
 
@@ -113,8 +162,15 @@ implements IWorkbenchPreferencePage, IPreferenceConstants {
         setSpinnerValues();
         fBackupOnSaveButton.setSelection(getPreferenceStore().getBoolean(BACKUP_ON_SAVE));
         fOpenDiagramsOnLoadButton.setSelection(getPreferenceStore().getBoolean(OPEN_DIAGRAMS_ON_LOAD));
-        fUseCurvedTabsButton.setSelection(!PlatformUI.getPreferenceStore().getBoolean(IWorkbenchPreferenceConstants.SHOW_TRADITIONAL_STYLE_TABS));
         fAnimateVisualiserNodesButton.setSelection(getPreferenceStore().getBoolean(ANIMATE_VISUALISER_NODES));
+
+        // Themes
+        List<ITheme> themes = fThemeEngine.getThemes();
+        fThemeComboViewer.setInput(themes.toArray());
+        ITheme activeTheme = fThemeEngine.getActiveTheme();
+        if(activeTheme != null) {
+            fThemeComboViewer.setSelection(new StructuredSelection(activeTheme));
+        }
     }
     
     private void setSpinnerValues() {
@@ -126,8 +182,13 @@ implements IWorkbenchPreferencePage, IPreferenceConstants {
         getPreferenceStore().setValue(BACKUP_ON_SAVE, fBackupOnSaveButton.getSelection());
         getPreferenceStore().setValue(OPEN_DIAGRAMS_ON_LOAD, fOpenDiagramsOnLoadButton.getSelection());
         getPreferenceStore().setValue(MRU_MAX, fMRUSizeSpinner.getSelection());
-        PlatformUI.getPreferenceStore().setValue(IWorkbenchPreferenceConstants.SHOW_TRADITIONAL_STYLE_TABS, !fUseCurvedTabsButton.getSelection());
         getPreferenceStore().setValue(ANIMATE_VISUALISER_NODES, fAnimateVisualiserNodesButton.getSelection());
+        
+        ITheme theme = (ITheme)((IStructuredSelection)fThemeComboViewer.getSelection()).getFirstElement();
+        if(theme != null) {
+            fThemeEngine.setTheme(theme, true);
+        }
+
         return true;
     }
     
@@ -136,11 +197,42 @@ implements IWorkbenchPreferencePage, IPreferenceConstants {
         fBackupOnSaveButton.setSelection(getPreferenceStore().getDefaultBoolean(BACKUP_ON_SAVE));
         fOpenDiagramsOnLoadButton.setSelection(getPreferenceStore().getDefaultBoolean(OPEN_DIAGRAMS_ON_LOAD));
         fMRUSizeSpinner.setSelection(getPreferenceStore().getDefaultInt(MRU_MAX));
-        fUseCurvedTabsButton.setSelection(!PlatformUI.getPreferenceStore().getDefaultBoolean(IWorkbenchPreferenceConstants.SHOW_TRADITIONAL_STYLE_TABS));
         fAnimateVisualiserNodesButton.setSelection(getPreferenceStore().getDefaultBoolean(ANIMATE_VISUALISER_NODES));
+        
+        fThemeEngine.setTheme(fDefaultTheme, true);
+        
+        ITheme activeTheme = fThemeEngine.getActiveTheme();
+        if(activeTheme != null) {
+            fThemeComboViewer.setSelection(new StructuredSelection(activeTheme));
+        }
+        
         super.performDefaults();
     }
     
-    public void init(IWorkbench workbench) {
+    @Override
+    public boolean performCancel() {
+        if(fCurrentTheme != fThemeEngine.getActiveTheme()) {
+            fThemeEngine.setTheme(fCurrentTheme, true);
+        }
+        
+        return super.performCancel();
     }
+    
+    public void init(IWorkbench workbench) {
+        // e4 method (taken from org.eclipse.ui.internal.dialogs.ViewsPreferencePage init(IWorkbench))
+        MApplication application = (MApplication)workbench.getService(MApplication.class);
+        IEclipseContext context = application.getContext();
+        fDefaultTheme = (String)context.get("cssTheme"); // This is "org.eclipse.e4.ui.css.theme.e4_default" //$NON-NLS-1$
+        fThemeEngine = context.get(IThemeEngine.class);
+        fCurrentTheme = fThemeEngine.getActiveTheme();
+/*
+        // e3 hacky method
+        Bundle bundle = FrameworkUtil.getBundle(ArchimateEditorPlugin.class);
+        BundleContext context = bundle.getBundleContext();
+        ServiceReference<IThemeManager> ref = context.getServiceReference(IThemeManager.class);
+        IThemeManager themeManager = context.getService(ref);
+        fThemeEngine = themeManager.getEngineForDisplay(Display.getCurrent());
+        fDefaultTheme = "org.eclipse.e4.ui.css.theme.e4_default"; //$NON-NLS-1$
+*/
+    }    
 }
